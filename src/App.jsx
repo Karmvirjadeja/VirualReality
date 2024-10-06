cabd47435bedfa0fb866e52c1bad63ab93e4c1fd
Karmvir <jadejakarmvir12@gmail.com>
2024-10-06 11:30:25 +0530

    Here added the video to the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
-      </div>
+      <div className="max-w-7xl mx-auto pt-20 px-6"></div>
+      <HeroSection />
     </div>
   );
 };
